2e7d87c296fdfcc1550ebf53792b0d773913b119
JoolsF <joolsmf@gmail.com>
2018-05-13 12:33:14 +0100

    Ch 3 continued

--- src/main/scala/chapters/Chapter3Functors.scala
+++ src/main/scala/chapters/Chapter3Functors.scala
@@ -3,6 +3,53 @@ package chapters
 /**
   * Functors
   */
+
+object Ch3Exercises {
+
+  import cats.Functor
+  import cats.syntax.functor._
+
+  // Write a Functor for the following binary tree data type.
+  // Verify that the code works as expected on instances of Branch and Leaf:
+  sealed trait Tree[+A]
+
+  final case class Branch[A](left: Tree[A], right: Tree[A])
+    extends Tree[A]
+
+  final case class Leaf[A](value: A) extends Tree[A]
+
+  implicit val treeFunc = new Functor[Tree] {
+    def map[A, B](fa: Tree[A])(f: A => B): Tree[B] = fa match {
+      case Branch(left, right) =>
+        Branch(map(left)(f), map(right)(f))
+      case Leaf(v) =>
+        Leaf(f(v))
+    }
+  }
+  val tree1 = Branch(
+    Leaf(1),
+    Leaf(2)
+  )
+
+  // tree1.map(_ * 2)
+  // this fails to compile with below as we only have Functor for Tree not Branch
+  // need to add smart constructors
+
+  object Tree {
+    def branch[A](left: Tree[A], right: Tree[A]): Tree[A] =
+      Branch(left, right)
+
+    def leaf[A](value: A): Tree[A] =
+      Leaf(value)
+  }
+
+  Tree.leaf(100).map(_ * 2) // Leaf(200)
+  Tree.branch(Tree.leaf(10), Tree.leaf(20)).map(_ * 2) //  Branch(Leaf(20),Leaf(40))
+
+
+}
+
+
 object Chapter3 {
   /**
     * Informally its anything with a map method.
@@ -43,20 +90,122 @@ object Chapter3 {
 
   (a andThen b) ("3") // List(3.0, 3.0, 3.0)
 
+  /**
+   Higher Kinded Types
+
+   https://typelevel.org/blog/2016/08/21/hkts-moving-forward.html
+   https://stackoverflow.com/questions/6246719/what-is-a-higher-kinded-type-in-scala
+   http://adriaanm.github.io/research/2010/10/06/new-in-scala-2.8-type-constructor-inference/
+   https://medium.com/bigpanda-engineering/understanding-f-in-scala-4bec5996761f
+
+    List    // type constructor, takes one parameter (WONT COMPILE)
+    List[A] // type, produced using a type parameter.  Also a 'first order type'.
+
+    analogous to
+
+     math.abs    // function, takes one parameter
+     math.abs(x) // value, produced using a value parameter
+
+
+    A type constructor is a function that takes a type and returns a type
+    List[_] is T => List[T]
+    It could be thought of as a type level function that works with types rather than values
+
+    1, "a", List(1,2,3) are values
+
+    Int, String, List[Int] are proper types
+
+    List[_], Option[_] are type constructors, takes a type and constructs a new type.
+    Can be generalized with this syntax F[_]
+
+    G[F[_]] is a type constructor that takes another type constructor like, Functor[F[_]],
+    can be thought of as a higher order function at the type level
+
+  */
 
   /**
     * Functors in Cats
+    * Cats definition of Functor allows us to create instances for any single-parameter type constructor,
+    * such as List, Option, Future, or a type alias such as MyFunc.
     */
 
   // Look like this.  Note this user higher kinded type syntax
-  trait Functor[F[_]] {
+  trait MyFunctor[F[_]] {
     def map[A, B](fa: F[A])(f: A => B): F[B]
+
+    // Cats functor basic example
+
+    import cats.Functor
+    import cats.instances.list._ // for Functor
+    import cats.instances.option._ // for Functor
+
+    val list1 = List(1, 2, 3)
+
+    Functor[List].map(list1)(_ * 3) //List(3, 6, 9)
+
+    val option1: Option[Double] = Some(Math.PI)
+
+    Functor[Option].map(option1)(_.toInt) //Some(3)
+
+
+    // Functor also has a lift function
+    // (A => B) => (F[A] => F[B])
+
+
+    val func1: Int => Int = (x: Int) => x + 1
+
+    val liftedFunc1: Option[Int] => Option[Int] = Functor[Option].lift(func1)
+
+    liftedFunc1(Some(1)) //Some(2)
+
+    // The main method provided by the syntax for Functor is map.
+    // Can be thought of as a way of appending a transformation to a chain
+    import cats.syntax.functor._ // for map
+
+    val f1 = (x: Int) => x * 2
+    val f2 = (x: Int) => (x, x * x)
+    val f3 = (x: (Int, Int)) => (s"${x._1.toString}!", x._2.toDouble)
+
+    val f1to3 = f1.map(f2).map(f3)
+    f1to3(1) //(String, Double) = (2!,4.0)
+
+
+    // applies equation to any functor no matter what the context is
+    def doMath[F[_]](start: F[Int])
+                    (implicit functor: Functor[F]): F[Int] =
+      start.map(n => n + 1 * 2)
+
+
+    doMath(Option(10))
+    doMath(List(10, 11))
+
+    //Instances for custom types
+
+    case class Foo[A](a: A) {
+      def transform[B](f: A => B): Foo[B] = Foo(f(a))
+    }
+
+    implicit val fooF = new Functor[Foo] {
+      def map[A, B](value: Foo[A])(func: A => B): Foo[B] =
+        value.transform(func)
+    }
+
+
+    Foo(1).map(_ + 2) //Foo 3
   }
 
+
+  /**
+    * Contravariant and Invariant Functors
+    *
+    * Functors map is a of appending a transformation to a chain.
+    * A contravariant functor represents prepending operations.
+    * Invariant functor represents a bidirectional chain of operations.
+    */
+
+
+
 }
 
-object Chapter4HigherKindedTypes {
-  // https://typelevel.org/blog/2016/08/21/hkts-moving-forward.html
-  // https://stackoverflow.com/questions/6246719/what-is-a-higher-kinded-type-in-scala
-  // http://adriaanm.github.io/research/2010/10/06/new-in-scala-2.8-type-constructor-inference/
-}
+
+
