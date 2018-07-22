19ecab6abb41697350eb62140dd7e8cdd4bec145
JoolsF <julian.fenner@ontrackretail.co.uk>
2018-07-22 19:46:45 +0100

    ch 4 monads continued

--- src/main/scala/chapters/Chapter4Monads.scala
+++ src/main/scala/chapters/Chapter4Monads.scala
@@ -15,7 +15,10 @@ object Chapter4Monads {
 
   def foo(s: String): Option[Int] = Try(s.toInt).toOption
 
-  foo("1").flatMap(a => foo("2").flatMap(b => foo("3").map(c => a + b + c)))
+  foo("1")
+    .flatMap(a => foo("2")
+      .flatMap(b => foo("3")
+        .map(c => a + b + c)))
   for {
     a <- foo("1")
     b <- foo("2")
@@ -35,13 +38,13 @@ object Chapter4Monads {
     *
     * Laws
     * - Left identity - calling pure and transforming the result with func is the same as calling func
-    *   pure(a).flatMap(func) == func(a)
+    * pure(a).flatMap(func) == func(a)
     *
     * - Right identity - passing pure to flatMap is the same as doing nothing
     *    m.flatMap(pure) == m
     *
     *  - Associativity - flatMapping over two functions f and g is the same as flatMapping over f and
-    *   then flatMapping over g
+    * then flatMapping over g
     *   m.flatMap(f).flatMap(g) == m.flatMap(x => f(x).flatMap(g))
     *
     */
@@ -55,9 +58,40 @@ object Chapter4Monads {
       flatMap(value)(a => pure(func(a)))
   }
 
+  trait MonadWithLaws[F[_]] extends Monad[F[_]] {
+
+    def flatMap[A, B](value: F[A])(func: A => F[B]): Boolean
+
+  }
+
   /**
     * Monads in Cats
     */
+  import cats.Monad
+  import cats.instances.option._
+  import cats.instances.list._
+
+  val opt1: Option[Int] = Monad[Option].pure(3) // Some(3)
+
+  val opt2: Option[Int] = Some(1).flatMap(i => Some(i * 2)) // Some(6)
+
+  val opt3 = Monad[Option].map(opt2)(_ * 3) //Some(18)
+
+  val list1 = Monad[List].pure(3) //List(3)
+
+  val list2 = Monad[List].flatMap(List(1, 2, 3))(i => List(i, i * 9)) //List(1, 9, 2, 18, 3, 27)
+
+  val list3 = Monad[List].flatMap(list2)(x => List(x * 10)) //List(10, 90, 20, 180, 30, 270)
+
+  /**
+    *
+    * Default instances
+    */
+
+
+  //import cats.instances.option._ // for Monad
+
+  Monad[Option].flatMap(Option(1))(a => Option(a*2))
 
 
 }
